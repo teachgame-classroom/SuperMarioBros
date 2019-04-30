78cede1968f243642cd7e9394e3581abeb7adf04
teachgame <teachgame@163.com>
2019-04-30 16:54:10 +0800

    修改跳跃逻辑

--- Assets/Mario.cs
+++ Assets/Mario.cs
@@ -7,11 +7,16 @@ public class Mario : MonoBehaviour
     [Range(0,20)]
     public float movePower = 10;
 
-    [Range(0, 1000)]
-    public float jumpPower = 1000;
+    [Range(0, 20)]
+    public float jumpMinPower = 20;
+
+    [Range(20, 30)]
+    public float jumpMaxPower = 25;
 
     private bool isBreaking;
     private bool isOnGround;
+
+    private Stopwatch jumpTimer = new Stopwatch(0.2f);
 
     // Start is called before the first frame update
     void Start()
@@ -22,47 +27,70 @@ public class Mario : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool isOnGround = CheckGround();
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         //Debug.Log("左右方向轴：" + h);
         //Debug.Log("上下方向轴：" + v);
 
-        Vector3 force = Vector3.right * h;
-
-        if(h > 0)
+        if (h > 0)
         {
             transform.rotation = Quaternion.identity;
         }
-        else if(h < 0)
+        else if (h < 0)
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
         }
 
+        Vector3 force = Vector3.right * h;
+
         GetComponent<Rigidbody2D>().AddForce(force * movePower);
-        float speed = GetComponent<Rigidbody2D>().velocity.magnitude;
 
-        GetComponent<Animator>().SetFloat("speed", speed);
 
-        if (ShouldBreak(GetComponent<Rigidbody2D>().velocity.x, h))
+        if (isOnGround)
         {
-            if (!isBreaking)
+            float speed = GetComponent<Rigidbody2D>().velocity.magnitude;
+
+            GetComponent<Animator>().SetFloat("speed", speed);
+
+            if (ShouldBreak(GetComponent<Rigidbody2D>().velocity.x, h))
             {
-                Debug.Log("Break");
-                isBreaking = true;
-                GetComponent<Animator>().ResetTrigger("break");
-                GetComponent<Animator>().SetTrigger("break");
+                if (!isBreaking)
+                {
+                    Debug.Log("Break");
+                    isBreaking = true;
+                    GetComponent<Animator>().ResetTrigger("break");
+                    GetComponent<Animator>().SetTrigger("break");
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                jumpTimer.Start();
+            }
+
+            if(Input.GetKeyUp(KeyCode.K))
+            {
+                if(jumpTimer.isCounting)
+                {
+                    jumpTimer.Stop();
+                    float jumpPower = Mathf.Lerp(jumpMinPower, jumpMaxPower, jumpTimer.time);
+                    GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+                }
+            }
+
+            if (jumpTimer.TimeSinceStart() > 0.17f)
+            {
+                if(jumpTimer.isCounting)
+                {
+                    jumpTimer.Stop();
+                    GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpMaxPower, ForceMode2D.Impulse);
+                }
             }
         }
 
-        if(Input.GetKeyDown(KeyCode.K))
-        {
-            if(isOnGround)
-            {
-                GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
-                GetComponent<Rigidbody2D>().gravityScale = 5;
-                isOnGround = false;
-            }
-        }
+        GetComponent<Animator>().SetBool("grounded", isOnGround);
     }
 
     bool ShouldBreak(float speed, float accerlation)
@@ -78,15 +106,24 @@ public class Mario : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    bool CheckGround()
     {
-        Debug.Log(collision.gameObject.tag);
+        bool result = false;
+        Physics2D.queriesStartInColliders = false;
+        GetComponent<Rigidbody2D>().gravityScale = 5;
 
-        if(collision.contacts[0].normal.y > 0)
+        bool frontHasGround = (Physics2D.Raycast(transform.position + Vector3.right * 0.5f, Vector2.down, 0.55f).transform != null);
+        bool backHasGround = (Physics2D.Raycast(transform.position - Vector3.right * 0.5f, Vector2.down, 0.55f).transform != null);
+        bool midHasGround = (Physics2D.Raycast(transform.position, Vector2.down, 0.55f).transform != null);
+
+        if (frontHasGround || backHasGround || midHasGround)
         {
-            Debug.Log("踩到地面");
+            result = true;
             GetComponent<Rigidbody2D>().gravityScale = 1;
-            isOnGround = true;
         }
+
+        Physics2D.queriesStartInColliders = true;
+
+        return result;
     }
 }
---
+++ Assets/Stopwatch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stopwatch
+{
+    public bool isCounting;
+
+    public float startTime;
+    public float endTime;
+
+    public float maxTime;
+
+    public float normalizedTime { get { return Mathf.Clamp01(time / maxTime); } }
+    public float time;
+
+    public Stopwatch(float maxTime = 1)
+    {
+        this.maxTime = maxTime;
+    }
+
+    public void Start()
+    {
+        startTime = Time.realtimeSinceStartup;
+        endTime = startTime;
+        isCounting = true;
+    }
+
+    public float Stop()
+    {
+        endTime = Time.realtimeSinceStartup;
+        time = endTime - startTime;
+        isCounting = false;
+        Debug.Log("Normalized:" + normalizedTime);
+        return time;
+    }
+
+    public float TimeSinceStart()
+    {
+        if(isCounting)
+        {
+            return Time.realtimeSinceStartup - startTime;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
